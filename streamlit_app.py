756b51f13ace34fd24d87e3c921310818a734cac
stimsonc <76088024+stimsonc@users.noreply.github.com>
2022-09-02 09:40:23 -0700

    Update streamlit_app.py
    provide default fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,5 @@ streamlit.text('🥑 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocaado', 'Strawberries'])
 streamlit.dataframe(my_fruit_list)
